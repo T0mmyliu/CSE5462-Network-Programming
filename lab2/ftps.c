d3d2fa843660b1c255b92ebf682f324d05175b7e
t0mmyliu <liuweipeng0001@gmail.com>
2016-09-09 11:43:55 -0400

    finish lab2: simple file transfer.

--- lab2/ftps.c
+++ lab2/ftps.c
@@ -8,63 +8,114 @@
 
 int process_request(int conn_sock);
 
-int main()
+//A multiprocess server
+int main(int argc,char* argv[])
 {
-		int listen_sock,conn_sock;
-		pid_t child_pid;
-		struct sockaddr_in serv_addr,cli_addr;
-		int cli_len;
-		int port;
+    int listen_sock,conn_sock;
+    pid_t child_pid;
+    struct sockaddr_in serv_addr,cli_addr;
+    socklen_t cli_len;
+    int port;
 
-		listen_sock = socket(AF_INET, SOCK_STREAM, 0);
-		
-		bzero(&serv_addr, sizeof(serv_addr));
-		serv_addr.sin_family = AF_INET;
-		serv_addr.sin_addr.s_addr = INADDR_ANY;
-		serv_addr.sin_port = htons(port);
+    port=atoi(argv[1]);
 
-		if(bind(listen_sock, (struct sockaddr*)& serv_addr, sizeof(struct sockaddr_in))<0){
-				printf("Error binding stream socket.\n");
-				return;
-		}
-		
-		listen(listen_sock, 5);
+    listen_sock = socket(AF_INET, SOCK_STREAM, 0);
 
-		for( ; ;){
-				cli_len = sizeof(cli_addr);
-				conn_sock = accept(listen_sock, (struct sockaddr*) &cli_addr, &cli_len);
+    bzero(&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(port);
 
-				if((child_pid=fork())==0){
-					close(listen_sock);
-					process_request(conn_sock);
-					exit(0);
-				}
-				close(conn_sock);
-		}
+    if(bind(listen_sock, (struct sockaddr*)& serv_addr, sizeof(struct sockaddr_in))<0){
+        perror("error binding stream socket");
+        return 0;
+    }
+
+    listen(listen_sock, 5);
+
+    //run server
+    for( ; ;){
+        printf("serveing...\n");
+        cli_len = sizeof(cli_addr);
+        conn_sock = accept(listen_sock, (struct sockaddr*) &cli_addr,&cli_len);
+
+        //process in child process
+        if((child_pid=fork())==0){
+            close(listen_sock);
+            process_request(conn_sock);
+            exit(0);
+        }
+        close(conn_sock);
+    }
 }
 
 int process_request(int conn_sock)
 {
-		size_t n;
-		char buf[1000],buf2[1000];
-		
-		bzero(buf,1000);
-		bzero(buf2,1000);
+    int file_len;
+    int left_len;
+    int rec_len;
+    char file_name[25];
+    char rec_name[40];
+    char buf[1000];
+    FILE* fp;
 
-		for(;;){
-				if ((read(conn_sock,buf,1000))<0){
-						printf("Error reading on stream socket\n");
-						exit(0);
-				}
-				
-				printf("Server receives: %s\n",buf);
+    bzero(&file_len,sizeof(file_len));
+    bzero(file_name,sizeof(file_name));
+    bzero(rec_name,sizeof(rec_name));
 
-				if((write(conn_sock,buf2,1000))<0){
-						printf("Error writing on stream socket\n");
-				}
+    //read the first four byte: file_len in network order.
+    rec_len=0;
+    while(rec_len!=4){
+        int tmp=0;
+        if((tmp=read(conn_sock,(void*)(&file_len),4))<0){
+            printf("Error reading on stream socket\n");
+            exit(0);
+        }
+        printf("read %d byte\n",tmp);
+        rec_len+=tmp;
+        printf("rec_len %d byte\n",rec_len);
+    }
+    file_len=ntohl(file_len);
 
-				printf("Server receives: %s\n",buf2);
+    printf("Server receives file length: %d\n",file_len);
 
-				close(conn_sock);
-		}
+    //read the following 25 byte: file_name.
+    rec_len=0;
+    while(rec_len!=25){
+        int tmp=0;
+        if((tmp=read(conn_sock,file_name,sizeof(file_name)))<0){
+            printf("Error writing on stream socket\n");
+        }
+        printf("read %d byte\n",tmp);
+        rec_len+=tmp;
+        printf("rec_len %d byte\n",rec_len);
+    }
+    printf("Server recevies file name: %s\n",file_name);
+    
+    //create new recve_file
+    strcat(rec_name,"recvd_");
+    strcat(rec_name,file_name);
+    if((fp=fopen(rec_name,"w"))==NULL){
+        perror("error create receive file.");
+    }
+    
+    left_len=file_len;
+    
+    //write data to recvd_file
+    fp=fopen(rec_name,"a");
+    while(left_len!=0){
+        bzero(buf,sizeof(buf));
+        if((rec_len=read(conn_sock,buf,sizeof(buf)))<0){
+            perror("error happen in receive.");
+        }
+        left_len-=rec_len;
+        
+        if((fwrite(buf,sizeof(char),rec_len,fp))!=rec_len){
+            perror("error happen when writing the data.");
+        }
+    }
+
+    close(conn_sock);
+    
+    return 0;
 }
